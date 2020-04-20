a9d8103b9bc1228149f775d578b19f85c855350e
Thekester <theophile.avenel@gmail.com>
2020-04-20 18:24:06 +0200

    Correction du problème de fontAwesome

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,66 +3,21 @@
 /* jslint white:true, browser:true, devel:true */
 
 
-var closable1 = document.getElementById('closable1');
-var closableConcernByClosable1 = document.getElementById('closableConcernByClosable1');
-var fleche1 = document.getElementById('fleche1');
-
-var closable2 = document.getElementById('closable2');
-var closableConcernByClosable2 = document.getElementById('closableConcernByClosable2');
-var fleche2 = document.getElementById('fleche2');
-
-var closable3 = document.getElementById('closable3');
-var closableConcernByClosable3 = document.getElementById('closableConcernByClosable3');
-var fleche3 = document.getElementById('fleche3');
 
 
+var blocExpert = document.getElementById('blocExpert');
+var newDivToProveExpert = document.createElement("div");
 
-//console.log(closable1);
-//sconsole.log(closableConcernByClosable1);
-
-closable1.addEventListener('click', function () {
+blocExpert.addEventListener('click', function () {
     "use strict";
-    //console.log("closable1.addEventListener "+closableConcernByClosable1.style.display);
-    if (closableConcernByClosable1.style.display === "block") {
-        closableConcernByClosable1.style.display = "none";
-        fleche1.className = "fas fa-angle-right fa-rotate-90";
-    } else if (closableConcernByClosable1.style.display === "") {
-        closableConcernByClosable1.style.display = "none";
-        fleche1.className = "fas fa-angle-right fa-rotate-90";
-    } else {
-        closableConcernByClosable1.style.display = "block";
-        fleche1.className = "fas fa-angle-right";
-
-    }
-});
-closable2.addEventListener('click', function () {
-    "use strict";
-    //console.log("closable1.addEventListener "+closableConcernByClosable1.style.display);
-    if (closableConcernByClosable2.style.display === "block") {
-        closableConcernByClosable2.style.display = "none";
-        fleche2.className = "fas fa-angle-right fa-rotate-90";
-    } else if (closableConcernByClosable2.style.display === "") {
-        closableConcernByClosable2.style.display = "none";
-        fleche2.className = "fas fa-angle-right fa-rotate-90";
-    } else {
-        closableConcernByClosable2.style.display = "block";
-        fleche2.className = "fas fa-angle-right";
-
-    }
+    newDivToProveExpert.style.position = 'relative';
+    newDivToProveExpert.style.zIndex = '10000';
+    newDivToProveExpert.backgroundColor = 'white';
+    newDivToProveExpert.style.display = 'inline-block';
+    newDivToProveExpert.textContent = 'test';
+    newDivToProveExpert.width = '40%';
+    newDivToProveExpert.height = 'auto';
+    document.add(newDivToProveExpert);
+    
 });
 
-closable3.addEventListener('click', function () {
-    "use strict";
-    //console.log("closable1.addEventListener "+closableConcernByClosable1.style.display);
-    if (closableConcernByClosable3.style.display === "block") {
-        closableConcernByClosable3.style.display = "none";
-        document.getElementById("fleche3").className = "fas fa-angle-right fa-rotate-90";
-    } else if (closableConcernByClosable3.style.display === "") {
-        closableConcernByClosable3.style.display = "none";
-        fleche3.className = "fas fa-angle-right fa-rotate-90";
-    } else {
-        closableConcernByClosable3.style.display = "block";
-        fleche3.className = "fas fa-angle-right";
-
-    }
-});
