b1661b529a6ef7424ed236b82c04e1167d491ae0
Avenel Théophile <theophile.avenel@gmail.com>
2022-06-30 11:44:21 +0200

    Nouvelle version du site web
    Amélioration de la rubrique compétences, Ajout des stages réalisés en DUT et ajout d'un descriptif de ma licence  suivi à l'université de bourgogne franche comtée.

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,23 +1,3 @@
 /* eslint-env browser */
 
 /* jslint white:true, browser:true, devel:true */
-
-
-//<i class="fas fa-minus-circle"></i>
-
-var blocExpert = document.getElementById('blocExpert');
-var newDivToProveExpert = document.createElement("div");
-
-blocExpert.addEventListener('click', function () {
-    "use strict";
-    newDivToProveExpert.style.position = 'relative';
-    newDivToProveExpert.style.zIndex = '10000';
-    newDivToProveExpert.backgroundColor = 'white';
-    newDivToProveExpert.style.display = 'inline-block';
-    newDivToProveExpert.textContent = 'test';
-    newDivToProveExpert.width = '40%';
-    newDivToProveExpert.height = 'auto';
-    document.add(newDivToProveExpert);
-    
-});
-
